f6c3e5094348cf2929e6adf6db444895430f3995
Richard Michael <rmichael@edgeofthenet.org>
2011-06-29 14:14:56 +0200

    More test debugging.

--- lib/point.rb
+++ lib/point.rb
@@ -6,13 +6,17 @@ module Geometry
 
     def ==(another_point)
       puts "another_point: #{another_point}"
-      puts "another_point.id: #{another_point.id}"
+      puts "another_point.object_id: #{another_point.object_id}" unless another_point == nil
       puts "self: #{self}"
-      puts "self.id: #{self.id}"
+      puts "self.object_id: #{self.object_id}"
       # if another_point == nil
       #   return false
       # end
       # x == another_point.x && y == another_point.y
+      puts "x: #{x}"
+      puts "y: #{y}"
+      puts "another_point.x: #{another_point.x}" unless another_point == nil
+      puts "another_point.y: #{another_point.y}" unless another_point == nil
       ( ! another_point == nil ) && ( x == another_point.x && y == another_point.y )
       # true
     end
--- test/sphere/volume_test.rb
+++ test/sphere/volume_test.rb
@@ -2,21 +2,25 @@ require 'test/unit'
 require 'geometry'
 # require 'test/helper'
 
+# Run only this with:
+# rake test TEST=test/sphere/volume_test.rb
+
+
 class VolumeTest < Test::Unit::TestCase
   include Geometry
 
-  def test_non_negative_radius
-    assert_raise RadiusMustBeNonNegative do
-      Sphere.new(-1)
-    end
-  end
+  # def test_non_negative_radius
+  #   assert_raise RadiusMustBeNonNegative do
+  #     Sphere.new(-1)
+  #   end
+  # end
 
-  def test_initialize_without_center_is_at_origin
-    sphere = Sphere.new(1)
+  # def test_initialize_without_center_is_at_origin
+  #   sphere = Sphere.new(1)
 
-    assert_equal sphere.center.x, 0
-    assert_equal sphere.center.y, 0
-  end
+  #   assert_equal sphere.center.x, 0
+  #   assert_equal sphere.center.y, 0
+  # end
 
   def test_initialize_with_center
     center = Point.new(1,1)
